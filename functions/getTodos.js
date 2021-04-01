a9336a3ed2b2d8dfc3e29e9494ead8955454afef
Daniel <daniel@brasileiro.ca>
2021-04-01 18:15:12 -0300

    Initial commit

---
+++ functions/getTodos.js
@@ -0,0 +1,26 @@
+const fetch = require('cross-fetch');
+const ENDPOINT = `https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v1`;
+const ASTRA_DB_KEYSPACE = process.env.ASTRA_DB_KEYSPACE;
+const TABLE_NAME = 'jamstack_todos';
+
+exports.handler = async (event, context) => {
+  const {path} = event;
+  const sessionId = path.split('getTodos/')[1];
+
+  try {
+    const response = await fetch(`${ENDPOINT}/keyspaces/${ASTRA_DB_KEYSPACE}/tables/${TABLE_NAME}/rows/${sessionId}`, {
+      headers: {
+        'x-cassandra-token': process.env.ASTRA_DB_APPLICATION_TOKEN,
+      }
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(await response.json())
+    };
+  } catch (e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(e)
+    };
+  }
+}
